9c7340fb274558d2bc5403c30bf7d719731eae3f
David Perdomo <dperdomo@imagegroup.ws>
2017-01-30 23:25:56 -0500

    Eloquent crear y update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,32 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use EloquentORM\User;
 
-Route::get('/', function () {
+Route::get('/create', function () {
+   
+	$user = User::create([
+		'name' => 'David Perdomo',
+		'email' =>'d@perdomo.com',
+		'password'=> bcrypt('123456'),
+		'gender'=>'m',
+		'biography' =>'El mejor en el mundo'
+	]);
+	
+	return "Usuario guardado";
+});
+
+Route::get('/update-user', function () {
+   
+	$user = User::find(1);
+	$user->gender = 'm';
+	$user->biography ='El mejor en el mundo';
+	$user->save();
+		
+	return "Usuario actualizado";
+});
+
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
